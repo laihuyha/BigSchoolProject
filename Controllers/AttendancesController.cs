1bb84a334282596e36a8f87cd7c4aa261c21687e
Lại Huy Hà <laihuyha@gmail.com>
2021-07-04 15:24:13 +0700

    No Commit

---
+++ Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BigSchoolProject.DTOs;
+using BigSchoolProject.Models;
+using Microsoft.AspNet.Identity;
+
+namespace BigSchoolProject.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext = new ApplicationDbContext();
+
+        public AttendancesController()
+        {
+
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
+            var attendance = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
